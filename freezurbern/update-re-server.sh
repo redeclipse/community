85a11607649487abd884de890a708ce5d38c888a
freezurbern <freezurbern@gmail.com>
2015-07-10 18:28:46 -0400

    Add waiting for server to exit
    Now with COLOUR! ...and more notifications.

--- freezurbern/update-re-server.sh
+++ freezurbern/update-re-server.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
+RED='\033[0;31m' # Red output
+YELLOW='\033[1;33m' # Yellow output
+GREEN='\033[0;32m' # Green output
+NC='\033[0m' # No Color
 
-cd redeclipse-git
+cd /home/zachery/redeclipse-git
 
+if [ "$1" == "start" ]; then
+    echo -e "${RED}Starting server${NC}"
+    nohup ./redeclipse_server.sh >> log.txt 2>&1 & echo $! > run.pid
+	echo -e "${GREEN}Red Eclipse recompiled and restarted.${NC}"
+    echo -e "${GREEN}==========================================${NC}"
+    exit 0
+fi
+
+echo -e "${RED}Downloading updates${NC}"
 git checkout master
 git pull
 git submodule update
+echo -e "${YELLOW}Updates received${NC}"
 
-REV=$(git rev-parse --short HEAD)
-echo "gitver = "$REV > gitver.cfg
+echo -e "${RED}Writing version information to file${NC}"
+  REV=$(git rev-parse --short HEAD)
+echo -e "gitver = "$REV > gitver.cfg
 
-make -C src/ clean
-rm bin/redeclipse_*
-make -C src/ -j2 server
-kill $(<"run.pid")
-make -C src/ install-server
+echo -e "${RED}Compiling server${NC}"
+  make -C src/ clean
+  rm bin/redeclipse_*
+  make -C src/ -j2 server
 
-REDECLIPSE_BRANCH="inplace"
-nohup ./redeclipse_server.sh >> log.txt 2>&1 & echo $! > run.pid
-echo "Red Eclipse recompiled and restarted."
+echo -e "${RED}Sending shutdown signal to server...${NC}"
+  kill $(<"run.pid")
+echo -e "${YELLOW}Signal sent. Waiting for server to shutdown...${NC}"
+  while [ -e /proc/$(<"run.pid") ]; do sleep 1; done
+echo -e "${YELLOW}... Server completed shutdown. Installing new binary.${NC}"
+  make -C src/ install-server
+echo -e "${YELLOW}Giving old server IRC bot time to disconnect. 10 seconds.${NC}"
+  sleep 10
+
+echo -e "${RED}Starting server...${NC}"
+  REDECLIPSE_BRANCH="inplace"
+  nohup ./redeclipse_server.sh >> log.txt 2>&1 & echo $! > run.pid
+echo -e "${GREEN}Red Eclipse recompiled and restarted.${NC}"
+echo -e "${GREEN}==========================================${NC}"
