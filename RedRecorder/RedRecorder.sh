e95957a8ee1d365f6d7a5f906a3d74dca7d59de4
srukle <srukle@gmx.com>
2015-09-08 10:30:53 -0500

    update to complete

--- RedRecorder/RedRecorder.sh
+++ RedRecorder/RedRecorder.sh
@@ -32,9 +32,21 @@ if [ -d "/home/$USER/RedRecorder/" ]; then
 			POS_ASSIST_SCORE_TWO="$(grep "helped by $NAME" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
 			POS_ASSIST_SCORE_THR="$(grep "and $NAME" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
 		### Negative Additives ###
-			NEG_FRAG_SCORE="$(grep "$NAME was" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
-			NEG_TEAM_SOCRE="$(grep "by team-mate $NAME" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
-			NEG_FRAG_SCORE="$(grep "$NAME was" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_TEAM_SCORE="$(grep "by team-mate $NAME" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE1="$(grep "$NAME was" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE2="$(grep "$NAME kicked" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE3="$(grep "$NAME died" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE4="$(grep "$NAME drowned" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE5="$(grep "$NAME had their head" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE6="$(grep "$NAME fell" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE7="$(grep "$NAME melted" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE8="$(grep "$NAME twitched" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE9="$(grep "$NAME gave up" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE10="$(grep "$NAME suicided" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE11="$(grep "$NAME met" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE12="$(grep "$NAME burned" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE13="$(grep "$NAME bled" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"
+			NEG_FRAG_SCORE14="$(grep "$NAME tried" /home/$USER/RedRecorder/.Logs/$NUMBER | wc -l)"			
 		### Total Positive ###
 			CUR_POS_ASSIST1="$(($POS_ASSIST_SCORE_ONE+$POS_ASSIST_SCORE_TWO))"
 			CUR_POS_ASSIST_TOTAL="$(($CUR_POS_ASSIST1+$POS_ASSIST_SCORE_THR))"
@@ -42,7 +54,20 @@ if [ -d "/home/$USER/RedRecorder/" ]; then
 			CUR_POS_SCORE="$(($POS_ASSIST+$POS_FRAG_SCORE))"
 			POS_SCORE="$(($CUR_POS_SCORE-CUR_POS_ASSIST_TOTAL))" ### Total
 		### Total Negative ###
-			NEG_SCORE="$(($NEG_FRAG_SCORE+$NEG_TEAM_SCORE))" ### Total
+			CUR_NEG_SCORE1="$(($NEG_FRAG_SCORE1+$NEG_FRAG_SCORE2))"
+			CUR_NEG_SCORE2="$(($CUR_NEG_SCORE1+$NEG_FRAG_SCORE3))"
+			CUR_NEG_SCORE3="$(($CUR_NEG_SCORE2+$NEG_FRAG_SCORE4))"
+			CUR_NEG_SCORE4="$(($CUR_NEG_SCORE3+$NEG_FRAG_SCORE5))"
+			CUR_NEG_SCORE5="$(($CUR_NEG_SCORE4+$NEG_FRAG_SCORE6))"
+			CUR_NEG_SCORE6="$(($CUR_NEG_SCORE5+$NEG_FRAG_SCORE7))"
+			CUR_NEG_SCORE7="$(($CUR_NEG_SCORE6+$NEG_FRAG_SCORE8))"
+			CUR_NEG_SCORE8="$(($CUR_NEG_SCORE7+$NEG_FRAG_SCORE9))"
+			CUR_NEG_SCORE9="$(($CUR_NEG_SCORE8+$NEG_FRAG_SCORE10))"
+			CUR_NEG_SCORE10="$(($CUR_NEG_SCORE9+$NEG_FRAG_SCORE11))"
+			CUR_NEG_SCORE11="$(($CUR_NEG_SCORE10+$NEG_FRAG_SCORE12))" 
+			CUR_NEG_SCORE12="$(($CUR_NEG_SCORE11+$NEG_FRAG_SCORE13))"
+			CUR_NEG_SCORE13="$(($CUR_NEG_SCORE12+$NEG_FRAG_SCORE14))"
+			NEG_SCORE="$(($CUR_NEG_SCORE13+$NEG_TEAM_SCORE))" ### Total
 		### Posting Total ###
 		echo -e "Your score for this session: ${GREEN}$POS_SCORE${NC} ${CYAN}/${NC} ${RED}$NEG_SCORE${NC}"
 		POS_ADD="$(cat /home/$USER/RedRecorder/.Scores/POSITIVE)"
